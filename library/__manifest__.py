5263be3ebf31a700d74a91812649325f99fec97a
Ana <anaceciliah95@gmail.com>
2019-05-06 09:37:15 -0400

    Creacion de moodulo desde cero en Odoo 12

---
+++ library/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+        Library Management""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Ana Hernandez",
+    'website': "https://github.com/ianahernandez",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    'installable': True,
+    # only loaded in demonstration mode
+
+}
